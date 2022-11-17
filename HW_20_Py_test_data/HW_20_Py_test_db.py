ca16fcf57b2bfa321117a9bd6b368405a6ede160
Asmod <Asmodians@hotmail.com>
2022-11-17 20:04:30 +0200

    HW_20_test_full

--- HW_20_Py_test_data/HW_20_Py_test_db.py
+++ HW_20_Py_test_data/HW_20_Py_test_db.py
@@ -5,19 +5,12 @@ import io
 from Homework.HW_19_Test_data.db import DataBase, DataBaseDTO, DataBaseException
 
 
-class TestDataBaseException(pytest):
+class TestDataBaseException:
     def setup(self) -> None:
         self.database_one_dto = DataBaseDTO('postgres', 'user', 'qwertyR1!', '127.0.0.1', '5001')
         self.database_one = DataBase(self.database_one_dto)
         self.table = 'table_12'
         self.data = 'Hello World'
-
-    def teardown(self) -> None:
-        del self.database_one
-
-    def test_delete_database_instance(self):
-        self.database_one.__del__()
-        assert DataBase.instance is None
 
     def test_singleton_database_pattern(self):
         data_mysql = DataBaseDTO('mysql', 'user', 'qwertyR1!', '127.0.0.1', '5001')
@@ -31,23 +24,23 @@ class TestDataBaseException(pytest):
     def test_connect(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             self.database_one.connect()
-            assert f'Connect to DB: {self.database_one.db_name}' == fake_out.getvalue()
+        assert f'Connect to DB: {self.database_one.db_name}' in fake_out.getvalue()
 
     def test_close_connect(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             self.database_one.close()
-            assert f'Close connect to DB: {self.database_one.db_name}' == fake_out.getvalue()
+        assert f'Close connect to DB: {self.database_one.db_name}' in fake_out.getvalue()
 
     def test_read_data(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             self.database_one.read(self.table)
-            self.assertTrue(f'Read data from database: {self.database_one.db_name} from table: {self.table}'
-                            in fake_out.getvalue())
+        assert f'Read data from database: {self.database_one.db_name} from table: {self.table}' \
+               in fake_out.getvalue()
 
     def test_write(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             self.database_one.write(self.table, self.data)
-            assert f'Write {self.data} to DB: {self.database_one.db_name} table: {self.table}' == fake_out.getvalue()
+        assert f'Write {self.data} to DB: {self.database_one.db_name} table: {self.table}' in fake_out.getvalue()
 
     def test_normal_data(self):
         assert self.database_one.db_name == self.database_one_dto.db_name
@@ -83,7 +76,7 @@ class TestDataBaseException(pytest):
         with pytest.raises(DataBaseException) as context_wrong_value:
             self.database_one.db_name = 'wrong'
         assert f'Unsupported DB: {"wrong"}. Use these names: {self.database_one.databases}' \
-               == str(context_wrong_value.value)
+               in str(context_wrong_value.value)
 
     def test_with_wrong_user(self):
         with pytest.warns(Warning, match="Use root user is dangerous"):
@@ -94,36 +87,36 @@ class TestDataBaseException(pytest):
             passwords = ['Admin#1', 'Admin_#one', 'admin_#1', 'ADMIN_#1', 'Adminnuber1']
             for password in passwords:
                 self.database_one.password = password
-        assert 'Password must be at least 8 chars include Upper, Lower, Digit, Punctuation' \
-               == str(context_wrong_password.value)
+            assert 'Password must be at least 8 chars include Upper, Lower, Digit, Punctuation' \
+                   in str(context_wrong_password.value)
 
     def test_with_wrong_host(self):
         with pytest.raises(DataBaseException) as context_wrong_host:
             self.database_one.host = '127.0.0.876'
-        assert "\'127.0.0.876\' does not appear to be an IPv4 or IPv6 address" == str(context_wrong_host.value)
+        assert "\'127.0.0.876\' does not appear to be an IPv4 or IPv6 address" in str(context_wrong_host.value)
 
         with pytest.raises(DataBaseException) as context_not_available_host:
             self.database_one.host = '192.168.88.99'
-        assert "192.168.88.99 is not available" == str(context_not_available_host.value)
+        assert "192.168.88.99 is not available" in str(context_not_available_host.value)
 
     def test_with_wrong_port(self):
         with pytest.raises(DataBaseException) as context_wrong_port:
             self.database_one.port = 'number_port'
-        assert 'Port must contains numbers not number_port' == str(context_wrong_port.value)
+        assert 'Port must contains numbers not number_port' in str(context_wrong_port.value)
 
         with pytest.raises(DataBaseException) as context_negative_port:
             self.database_one.port = '0'
-        assert f'Port must be between 0-65000' == str(context_negative_port.value)
+        assert f'Port must be between 0-65000' in str(context_negative_port.value)
 
         with pytest.raises(DataBaseException) as context_max_port:
             self.database_one.port = '66000'
-        assert f'Port must be between 0-65000', str(context_max_port.value)
+        assert f'Port must be between 0-65000' in str(context_max_port.value)
 
     def test_database_context(self):
         with patch('sys.stdout', new=io.StringIO()) as fake_out:
             with DataBase(self.database_one_dto) as db:
                 db.write('Owner', 'Some data')
-            assert fake_out.getvalue() == 'Connect to DB: postgres\n'\
+            assert fake_out.getvalue() in 'Connect to DB: postgres\n'\
                                           'Write Some data to DB: postgres table: Owner\n'\
                                           'Close connect to DB: postgres\n'
 
